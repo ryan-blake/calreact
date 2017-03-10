cf9f11b25c9d83a7bb712208c88f7dfb53a88a5c
Ryan Kliemann <Ryan_blake@icloud.com>
2017-03-09 22:25:07 -0600

    commit for heroku

---
+++ app/assets/javascripts/components/appointment.jsx
@@ -0,0 +1,10 @@
+var Appointment = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h3>{this.props.appointment.title}</h3>
+        <p>{formatDate(this.props.appointment.appt_time)}</p>
+      </div>
+    )
+  }
+})
